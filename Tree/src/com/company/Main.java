b93ac89cd0af63d7d79b2c201f8e103ba4389b92
Roman <student.rv86@gmail.com>
2019-02-21 12:03:30 +0200

    21.02

--- Tree/src/com/company/Main.java
+++ Tree/src/com/company/Main.java
@@ -1,31 +1,56 @@
-package com.company;
+//package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        MyTree tree = new MyTree();
-
-        tree.add(15);
-        tree.add(9);;
-        tree.add(19);
-
-
-        printTree(tree.getRoot());
-
-        tree.add(11);
-
-//        printTree(tree.getRoot());
-
-//        Node root = new Node(9);
-////        Node node1 = new Node(7);
-////        Node node2 = new Node(11);
-////
-////        root.left = node1;
-////        root.right = node2;
-
-       // printTree(root);
+        
+		MyTree tree = new MyTree();
+		int[] values = {15, 9, 19, 11, 21, 7, 17};
+		for (int i = 0; i < values.length; i++) {
+			tree.add(values[i]);
+		}
+		System.out.println("tree:");
+		printTree(tree.getRoot());
+		System.out.println();
+		
+        Node root = new Node(9);
+        Node node1 = new Node(7);
+        Node node2 = new Node(11);
+        root.left = node1;
+        root.right = node2;
+		
+		//Node node3 = new Node(5);
+		//node1.left = node3;
+		//Node node4 = new Node(8);
+		//node1.right = node4;
+		
+		System.out.println("root:");
+		printTree(root);
+		System.out.println();
+		
+		Node root2 = new Node(10);
+		root2.left = new Node(6);
+		root2.right = new Node(14);
+		System.out.println("root2:");
+		printTree(root2);
+		System.out.println();
+		sumTree(root, root2);
+		System.out.println();
+		printTree(root);
+		
     }
+	public static void sumTree(Node node1, Node node2) {
+		//result =>node1
+		
+		if (node1 == null && node2 == null) {
+			return;
+		}
+		node1.value += node2.value;
+		
+		sumTree(node1.left, node2.left);
+		sumTree(node1.right, node2.right);	
+	}
+	
     public static void printTree(Node node) {
         if (node == null) {
             return;
--- Tree/src/com/company/MyTree.java
+++ Tree/src/com/company/MyTree.java
@@ -1,31 +1,39 @@
-package com.company;
+//package com.company;
 
 public class MyTree {
-    private int size;
+ 
     private Node root;
 
     public void add(int value) {
-        Node newNode = new Node(value);
         if (root == null) {
-            root = newNode;
-        } else if (newNode.value > root.value) {
-
-            Node tmp = root.right;
-            while (tmp.right.value < newNode.value) {
-                tmp = tmp.right;
-                //if (tmp.left)
-            }
-
-
-
-//            root.right = newNode;
-//            newNode.parent = root;
-        } else if (newNode.value < root.value){
-            root.left = newNode;
-            newNode.parent = root;
-        }
-        size++;
+           root = new Node(value);
+        } 
+		else {
+			addInTree(root, value);
+		}   
     }
+	
+	private void addInTree(Node node, int value) {
+		Node newNode = new Node(value);
+		if (value > node.value) {
+			if (node.right == null) {
+				node.right = newNode;
+				return;
+			}
+			else {
+				addInTree(node.right, value);
+			}
+		}
+		if (value < node.value) {
+			if (node.left == null) {
+				node.left = newNode;
+				return;
+			}
+			else {
+				addInTree(node.left, value);
+			}
+		}	
+	}
 
     public Node getRoot() {
         return root;
